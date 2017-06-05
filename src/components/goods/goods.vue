ca4b077decef00c2f2e5b894d3f02560eaac1fcb
mnggle <muggle6594@gmail.com>
2017-06-05 14:56:32 +0800

    goods component update

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -4,14 +4,37 @@
       <ul>
         <li v-for="item in goods" class="menu-item">
           <span class="text border-1px">
-            <span v-show="item.type>0" class="icon" :class=""></span>
+            <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
             {{item.name}}
           </span>
         </li>
       </ul>
     </div>
     <div class="foods-wrapper">
-
+        <ul>
+          <li v-for="item in goods" class="food-list">
+            <h1 class="title">{{item.name}}</h1>
+            <ul>
+              <li v-for="food in item.foods" class="food-item border-1px">
+                <div class="icon">
+                  <img :src="food.icon" width="57" heigth="57">
+                </div>
+                <div class="content">
+                  <h2 class="name">{{food.name}}</h2>
+                  <p class="desc">{{food.description}}</p>
+                  <div class="extra">
+                    <span>月售{{food.sellCount}}份</span>
+                    <span>好评率{{food.rating}}%</span>
+                  </div>
+                  <div class="price">
+                    <span>￥{{food.price}}</span>
+                    <span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
     </div>
   </div>
 </template>
@@ -29,6 +52,7 @@
       };
     },
     created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
       this.$http.get('/api/goods').then((response) => {
         response = response.body;
         console.log(response);
@@ -61,6 +85,23 @@
         height:54px
         width:56px;
         line-height:14px
+        .icon
+          display:inline-block
+          vertical-align:top
+          width:12px
+          height:12px
+          background-size:12px 12px
+          background-repeat:no-repeat
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
         .text
           display:table-cell
           width:56px
@@ -69,4 +110,33 @@
           font-size:12px
     .foods-wrapper
       flex:1
+      .title
+        padding-left:14px
+        height:26px
+        line-height:26px
+        border-left:2px solid #d9dde1
+        font-size:12px
+        color:rgb(147,153,159)
+        background:#f3f5f7
+      .food-item
+        display:flex
+        margin:18px
+        padding-bottom:18px
+        border-1px(rgba(7,17,27,0.1))
+        &:last-child
+          border-none()
+          margin-bottom:0
+        .icon
+          flex:0 0 57px
+          margin-rigth:10px
+        .content
+          flex:1
+          .name
+            margin:2px 0 8px 0
+            height:14px
+            line-height:14px
+            font-size:14px
+            color:rgb(7,17,27)
+          
+          
 </style>
