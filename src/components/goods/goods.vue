a553647dfcacc63efae8048640dabf093668c2b4
mnggle <muggle6594@gmail.com>
2017-06-26 14:37:16 +0800

    better-scroll

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" v-el:menu-wrapper>
       <ul>
         <li v-for="item in goods" class="menu-item">
           <span class="text border-1px">
@@ -10,9 +10,9 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" v-el:foods-wrapper>
         <ul>
-          <li v-for="item in goods" class="food-list">
+          <li v-for="item in goods" class="food-list food-List-hook">
             <h1 class="title">{{item.name}}</h1>
             <ul>
               <li v-for="food in item.foods" class="food-item border-1px">
@@ -23,12 +23,12 @@
                   <h2 class="name">{{food.name}}</h2>
                   <p class="desc">{{food.description}}</p>
                   <div class="extra">
-                    <span>月售{{food.sellCount}}份</span>
+                    <span class="count">月售{{food.sellCount}}份</span>
                     <span>好评率{{food.rating}}%</span>
                   </div>
                   <div class="price">
-                    <span>￥{{food.price}}</span>
-                    <span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                    <span class="now">￥{{food.price}}</span>
+                    <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                   </div>
                 </div>
               </li>
@@ -39,6 +39,8 @@
   </div>
 </template>
 <script>
+  import BScroll from 'better-scroll';
+
   const ERR_OK = 0;
   export default {
     prop: {
@@ -48,20 +50,57 @@
     },
     data() {
       return {
-        goods: []
+        goods: [],
+        listHeight: [],
+        scrollY: 0
       };
+    },
+    computed: {
+      currentIndex () {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+          if (!height2 || this.scrollY > height1 && this.scrollY < height2) {
+            console.log(i);
+            return i;
+          };
+          return 0;
+        }
+      }
     },
     created() {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
       this.$http.get('/api/goods').then((response) => {
         response = response.body;
-        console.log(response);
+        // console.log(response);
         if (response.errno === ERR_OK) {
           this.goods = response.data;
+          this.$nextTick(() => {
+            this._initScroll();
+            this._caculateHeight();
+          });
         };
       }, response => {
         // error callback
       });
+    },
+    methods: {
+      _initScroll() {
+        this.menuScroll = new BScroll(this.$els.foodsWrapper, {});
+        this.foodsScroll = new BScroll(this.$els.menuWrapper, {
+          probeType: 3
+        });
+      },
+      _caculateHeight() {
+        var foodList = this.$els.foodsWrapper.getElementsByClassName('food-List-hook');
+        let height = 0;
+        this.listHeight.push(height);
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i];
+          height += item.clientHeight;
+          this.listHeight.push(height);
+        };
+      }
     }
   };
 </script>
@@ -128,7 +167,7 @@
           margin-bottom:0
         .icon
           flex:0 0 57px
-          margin-rigth:10px
+          margin-right:10px
         .content
           flex:1
           .name
@@ -137,6 +176,26 @@
             line-height:14px
             font-size:14px
             color:rgb(7,17,27)
-          
+          .desc, .extra
+            line-height:10px
+            font-size:10px
+            color:rgb(147,153,159)
+          .desc
+            margin-bottom:8px
+            line-height:12px
+          .extra
+            Count
+              margin-right:12px
+          .price
+            font-weight:700
+            line-height:24px
+            .now
+              margin-right:18px
+              font-size:14px
+              color:rgb(240,20,20)
+            .old
+              text-decoration:line-through
+              font-size:10px
+              color:rgb(147,153,159)
           
 </style>
