20f0e5f6c1bace4deb00a0f6bc131bc82377a897
mnggle <muggle6594@gmail.com>
2017-06-26 15:58:38 +0800

    better-scroll

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,7 +2,7 @@
   <div class="goods">
     <div class="menu-wrapper" v-el:menu-wrapper>
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="item in goods" class="menu-item" :class="{'current': currentIndex===$index}">
           <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
             {{item.name}}
@@ -12,7 +12,7 @@
     </div>
     <div class="foods-wrapper" v-el:foods-wrapper>
         <ul>
-          <li v-for="item in goods" class="food-list food-List-hook">
+          <li v-for="item in goods" class="food-list food-list-hook">
             <h1 class="title">{{item.name}}</h1>
             <ul>
               <li v-for="food in item.foods" class="food-item border-1px">
@@ -60,7 +60,7 @@
         for (let i = 0; i < this.listHeight.length; i++) {
           let height1 = this.listHeight[i];
           let height2 = this.listHeight[i + 1];
-          if (!height2 || this.scrollY > height1 && this.scrollY < height2) {
+          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
             console.log(i);
             return i;
           };
@@ -90,9 +90,12 @@
         this.foodsScroll = new BScroll(this.$els.menuWrapper, {
           probeType: 3
         });
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+        });
       },
       _caculateHeight() {
-        var foodList = this.$els.foodsWrapper.getElementsByClassName('food-List-hook');
+        let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
         let height = 0;
         this.listHeight.push(height);
         for (let i = 0; i < foodList.length; i++) {
@@ -124,6 +127,14 @@
         height:54px
         width:56px;
         line-height:14px
+        &.current
+          position:relative
+          z-index 10
+          margin-top:-1px
+          background:#fff
+          font-weight:700
+          .text
+            border-none()
         .icon
           display:inline-block
           vertical-align:top
